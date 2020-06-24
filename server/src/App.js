4759f511b21d6a9736f19bac5c281b504ab06ccd
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-06-24 19:32:52 -0300

    feat: base server

---
+++ server/src/App.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import morgan from 'morgan'
+
+// import './app/database'
+// import routes from './routes'
+// import { error } from './app/middlewares'
+
+export default class App {
+	constructor() {
+		this.express = express()
+
+		this.configs()
+		this.middlewares()
+		this.routes()
+	}
+
+	get app() {
+		return this.express
+	}
+
+	configs() {
+		this.express.disable('x-powered-by')
+	}
+
+	middlewares() {
+		const { CLIENT_HOST, NODE_ENV } = process.env
+
+		if (NODE_ENV === 'production') {
+			this.express.use(cors({ origin: CLIENT_HOST }))
+		} else if (NODE_ENV === 'development' || NODE_ENV === 'test') {
+			this.express.use(cors({ origin: '*' }))
+		}
+
+		this.express.use(helmet())
+		this.express.use(express.json())
+		this.express.use(morgan('dev'))
+	}
+
+	routes() {
+		// this.express.use(routes)
+		// this.express.use(error)
+	}
+}
---
+++ server/src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+// import * as controllers from './app/controllers'
+// import * as middlewares from './app/middlewares'
+// import * as validators from './app/validators'
+
+const router = Router()
+
+export default router
