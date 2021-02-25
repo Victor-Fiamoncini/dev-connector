025513975e9d054b96800fefe55b2fb5ba5cd0b3
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-25 01:27:13 -0300

    feat: add domain errors

--- server/src/data/services/CreateUserService.ts
+++ server/src/data/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { CreateUserRepository, CreateUserDTO } from '@data/repositories'
 import { User } from '@domain/entities'
+import { UserAlreadyExistsError } from '@domain/errors'
 import { CreateUserUseCase } from '@domain/usecases'
 
 export class CreateUserService implements CreateUserUseCase {
@@ -7,6 +8,7 @@ export class CreateUserService implements CreateUserUseCase {
 
 	async createUser(userData: CreateUserDTO): Promise<User> {
 		// Find/verify user with email...
+		throw new UserAlreadyExistsError()
 
 		// Insert user...
 		return this.createUserRepository.createUser(userData)
