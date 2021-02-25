35989e07318cba14edc7b7a5a32ee1eb25d8927b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-25 02:53:18 -0300

    feat: add main layer with express

--- server/src/infra/repositories/MongoCreateUserRepository.ts
+++ server/src/infra/repositories/MongoCreateUserRepository.ts
@@ -1,23 +1,18 @@
 import { CreateUserDTO, CreateUserRepository } from '@data/contracts'
 import { UserModel } from '@data/models'
 import { UserMongoDataSource } from '@infra/data-sources'
-import { UnexpectedError } from '@shared/errors'
 
 export class MongoCreateUserRepository implements CreateUserRepository {
 	async createUser(userData: CreateUserDTO): Promise<UserModel> {
-		try {
-			const createdUser = await UserMongoDataSource.create(userData)
+		const createdUser = await UserMongoDataSource.create(userData)
 
-			const { email, name, password, avatar } = createdUser
+		const { email, name, password, avatar } = createdUser
 
-			return {
-				name,
-				email,
-				password,
-				avatar,
-			}
-		} catch {
-			throw new UnexpectedError('creating an user with mongo database')
+		return {
+			name,
+			email,
+			password,
+			avatar,
 		}
 	}
 }
